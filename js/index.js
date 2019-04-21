ec279447db5783c08ad44eb2307e2395885d22db
apple872828 <38729407+apple872828@users.noreply.github.com>
2019-04-21 21:44:06 +0800

    第一次上傳
    第一次上傳

---
+++ js/index.js
@@ -0,0 +1,102 @@
+// 切換LEATHER專案
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("coll-slider");
+  var y = document.getElementsByClassName("cw-change");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+   if (n > y.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = y.length}
+  for (i = 0; i < y.length; i++) {
+     y[i].style.display = "none";  
+  }
+  y[slideIndex-1].style.display = "block";  
+  
+}
+// 切換CYGC專案
+var slideIndex1 = 1;
+showDivs1(slideIndex);
+
+function plusDivsf2(n) {
+  showDivs1(slideIndex1 += n);
+}
+
+function showDivs1(n) {
+  var i;
+  var x = document.getElementsByClassName("coll-sliderf2");
+  var y = document.getElementsByClassName("cw-changef2");
+  if (n > x.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex1-1].style.display = "block";  
+   if (n > y.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = y.length}
+  for (i = 0; i < y.length; i++) {
+     y[i].style.display = "none";  
+  }
+  y[slideIndex1-1].style.display = "block";  
+  
+}
+// 切換前端專案
+function changefro(n) {
+  var i;
+  var x = document.getElementsByClassName("cwf-pro");
+  
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[n].style.display = "block";   
+  
+}
+
+// 切換CLOUDY專案
+var slideIndexb1 = 1;
+showDivsb1(slideIndexb1);
+
+function plusDivsb1(n) {
+  showDivsb1(slideIndexb1 += n);
+}
+
+function showDivsb1(n) {
+  var i;
+  var x = document.getElementsByClassName("coll-sliderb1");
+  var y = document.getElementsByClassName("cw-changeb1");
+  if (n > x.length) {slideIndexb1 = 1}
+  if (n < 1) {slideIndexb1 = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndexb1-1].style.display = "block";  
+   if (n > y.length) {slideIndexb1 = 1}
+  if (n < 1) {slideIndexb1 = y.length}
+  for (i = 0; i < y.length; i++) {
+     y[i].style.display = "none";  
+  }
+  y[slideIndexb1-1].style.display = "block";  
+  
+}
+//切換前後端
+function changeworks(n) {
+  var i;
+  var x = document.getElementsByClassName("cw-front");
+
+  
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[n].style.display = "block";   
+  
+}
